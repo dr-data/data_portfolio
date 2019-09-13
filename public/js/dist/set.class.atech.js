bbd43cd3efa606549a8d4ead032fdc52ab5f5acb
dr-data <data@hku.hk>
2019-09-13 23:43:50 +0800

    Init gh-pages

--- public/js/dist/set.class.atech.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll("img").forEach(function(c){c.className+=" atechImg"});
--- public/js/src/set.class.atech.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	var getEveryImg = document.querySelectorAll('img');
-	getEveryImg.forEach(function(thisImg)
-	{
-		thisImg.className += ' ' + 'atechImg'
-	});
-})();
